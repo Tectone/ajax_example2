f4c1235132f0c9cd6c7431a9dde515440d918f2b
Tectone <tectone18@gmail.com>
2018-10-24 11:58:53 -0400

    dev.jrr.classUpdate

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
 $user = "root";
-$password = "root"; // windows users leave this blank
+$password = "root";
 $host = "localhost";
-$db = "cooperInfo";
+$db = "db_car_mini";
 
 $conn = mysqli_connect($host, $user, $password, $db);
 mysqli_set_charset($conn, 'utf8');
@@ -12,17 +12,33 @@ if (!$conn) {
     exit;
 }
 
+//echo "connected!";
+
 // get all the car data
-$myQuery = "SELECT * FROM mainmodel";
+// $myQuery = "SELECT * FROM mainmodel";
 
-// make the query, get the result
-$result = mysqli_query($conn, $myQuery);
+// // make the query, get the result
+// $result = mysqli_query($conn, $myQuery);
 
-$rows = array();
+// $rows = array();
 
-while($row = mysqli_fetch_assoc($result)) {
-    $rows[] = $row;
+// while($row = mysqli_fetch_assoc($result)) {
+//     $rows[] = $row;
+// }
+
+if (isset($_GET["carModel"])) {// check for a parameter ?carModel=R58
+    $car = $_GET["carModel"];
+
+    $myQuery = "SELECT * FROM mainmodel WHERE model = '$car'";
+
+    $result = mysqli_query($conn, $myQuery);
+    $rows = array();
+
+    while($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
 }
+
 // send the entire result set as a json encoded array
 echo json_encode($rows);
 
